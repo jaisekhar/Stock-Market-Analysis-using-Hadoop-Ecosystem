682ba3747b2a8dcb4f2a35dd30faee18c37ec4ec
SriSaiNikhilKantipudi <54340600+SriSaiNikhilKantipudi@users.noreply.github.com>
2020-12-06 21:43:55 -0600

    update

--- SourceCode/Spark SQL/src/main/scala/project.scala
+++ SourceCode/Spark SQL/src/main/scala/project.scala
@@ -18,12 +18,47 @@ object project {
     df.createOrReplaceTempView("dataset")
     df1.createOrReplaceTempView("dataset1")
 
+    // 1. Total no of tweets
+    val q1=SQLContext.sql("select count(*) from dataset")
+    q1.show()
 
-    val t1=System.nanoTime()
+    // 2. Tweets from different countries
+    val q2 = SQLContext.sql("SELECT place.country,count(*) AS count FROM dataset GROUP BY place.country ORDER BY count DESC limit 10")
+    q2.show()
+
+    // 3. Number of tweets based on day
+    val q3 = SQLContext.sql("SELECT substring(created_at,1,3) as day,count(*) as count from dataset group by substring(created_at,1,3)")
+    q3.show()
+
+    // 4. Users with more tweets
+    val q4=SQLContext.sql("SELECT count(*) as count, user.name from dataset where user.name is not null group by user.name order by count desc limit 10")
+    q4.show()
+
+    // 5. Users with more followers
+    val q5 = SQLContext.sql("SELECT user.screen_name, max(user.followers_count) as followers_count FROM dataset WHERE text like '%AAPL%' group by user.name order by followers_count desc limit 15");
+    q5.show()
+
+    // 6. Number of tweets based on months
+    val q6 = SQLContext.sql("SELECT substring(created_at,5,3) as month, count(*) as count from dataset group by substring(created_at,5,3)");
+    q6.show()
+
+    // 7. Number of tweets based on different locations in USA
+    val q7=SQLContext.sql("SELECT user.location,count(*) as count FROM dataset WHERE place.country='United States' AND user.location is not null GROUP BY user.location ORDER BY count DESC LIMIT 15");
+    q7.show()
+
+    // 8. Polarity of tweets
+    val q8=SQLContext.sql("select _c5 as polarity from dataset1 limit 10")
+    q8.show()
+
+    // 9. User Screen name with positive polarity
+    val q9=SQLContext.sql("select _c3 as screenname,_c5 as polarity from dataset1 where _c5>0.0")
+    q9.show()
+
     // 10. User name and total number of tweets  in descending order
+    val t1=System.nanoTime()
     val q10=SQLContext.sql("select user.name, count(*) as count from dataset group by user.name order by count desc limit 10")
     q10.show()
-    print("Runtime: " + (System.nanoTime()-t1)/1000/1000 + " ms")
+    print("Runtime" +(System.nanoTime()-t1)/1000/1000 + "ms")
 
 
 /*    println("Aggregate Function: AVG")
